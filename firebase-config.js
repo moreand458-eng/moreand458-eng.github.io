beb00d363e521ec4ffbc7db95d32d5e1261a037f
moreand458-eng <moreand458@gmail.com>
2026-05-05 22:02:03 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAS4IeaNNKsc0uG4tPy2T79LMqGCRWe1bU",
+  authDomain: "escanor-programming.firebaseapp.com",
+  databaseURL: "https://escanor-programming-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "escanor-programming",
+  storageBucket: "escanor-programming.firebasestorage.app",
+  messagingSenderId: "248564426785",
+  appId: "1:248564426785:web:7861511f45982f72391b60",
+  measurementId: "G-2QEHNLE0CK"
+};
